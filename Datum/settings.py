4eb86a170bf0db0adad0e43f26445728512903b7
Alessandro Perotti <palexander82@gmail.com>
2025-07-20 11:25:50 +0200

    Updated to add .gitignore

--- Datum/settings.py
+++ Datum/settings.py
@@ -94,12 +94,24 @@ WSGI_APPLICATION = 'Datum.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
+from decouple import config
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+        'OPTIONS': {
+            'connect_timeout': 20,
+        }
     }
 }
+
+
 
 
 # Password validation
